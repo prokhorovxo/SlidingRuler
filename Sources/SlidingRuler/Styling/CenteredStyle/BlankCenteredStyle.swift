2a2d1f381e0ca4afaa815dd3456a48dad50af714
Fedor Prokhorov <prokhorovxo@gmail.com>
2023-06-04 14:40:19 +0300

    Add missing public init to BlankCenteredStyle

--- Sources/SlidingRuler/Styling/CenteredStyle/BlankCenteredStyle.swift
+++ Sources/SlidingRuler/Styling/CenteredStyle/BlankCenteredStyle.swift
@@ -30,7 +30,10 @@
 import SwiftUI
 
 public struct BlankCenteredSlidingRulerStyle: SlidingRulerStyle {
+    
     public let cursorAlignment: VerticalAlignment = .top
+    
+    public init() {}
 
     public func makeCellBody(configuration: SlidingRulerStyleConfiguation) -> some FractionableView {
         BlankCenteredCellBody(mark: configuration.mark,
